4bc5440fe04ff04963996d645d74e8e03b006ca4
Nizom Sidiq <nizomsidiq@gmail.com>
2018-12-10 15:05:03 +0700

    perbaiki rumus

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const THRESHOLD = 0.8;
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((req, res) => {
-  res.send("Hello from Firebase!");
-});
+// exports.helloWorld = functions.https.onRequest((req, res) => {
+//   res.send("Hello from Firebase!");
+// });
 
 exports.solve = functions.https.onRequest((req, res) => {
   const app = admin.initializeApp(functions.config().firebase);
@@ -15,25 +15,32 @@ exports.solve = functions.https.onRequest((req, res) => {
   const db = admin.database();
   const gejala = req.body.gejala;
 
+  // res.send(gejala);
+
   db.ref('kasus').on('value',  (snapKasus) => {
     var dbKasus = snapKasus.val();
 
     db.ref('gejala').on('value', (snapGejala) => {
       var dbGejala = snapGejala.val();
-      
+      var totalGejala = gejala.reduce((tgr, tgg) => dbGejala[tgg].bobot + tgr, 0);
       computedKasus = Object.keys(dbKasus).map((i) => {
         var kasus = dbKasus[i];
         kasus.total = kasus.gejala.reduce((rTotal,gTotal) => dbGejala[gTotal].bobot + rTotal, 0);
-        kasus.mirip = kasus.gejala
-                          .filter(gMirip => gejala.includes(gMirip))
-                          .reduce((rMirip2,gMirip2) => dbGejala[gMirip2].bobot + rMirip2, 0);
-        kasus.result = kasus.mirip / kasus.total;
+        kasus.mirip = 
+          kasus.gejala
+            .filter(gMirip => gejala.includes(gMirip))
+            .reduce((rMirip2,gMirip2) => dbGejala[gMirip2].bobot + rMirip2, 0);
+
+        var pembagi = kasus.gejala.total > totalGejala ? kasus.gejala.total : totalGejala;
+        kasus.result = kasus.mirip / pembagi;
         return kasus;
       })
 
       // kasusTermirip = Object.keys(computedKasus).reduce((a, b) => { return computedKasus[a].result > computedKasus[b].result ? a : b; })
-      kasusTermirip = computedKasus.reduce((a,b) => a.result > b.result ? a : b);
-      console.log(computedKasus);
+      kasusTermirip = computedKasus.reduce((a,b) => { 
+        return a.result > b.result ? a : b
+      }, {result: 0});
+      // console.log(computedKasus);
       return deleteApp().then(() => res.send(kasusTermirip));
     })
 
